bfc039e0d0ac60d5a867294af32386e650b2d5e1
sumit <mazumdarsumit3@gmail.com>
2024-07-06 10:54:18 +0530

    variable and constant in rust

---
+++ 02-var/constants.rs
@@ -0,0 +1,5 @@
+fn main(){
+    // constants
+    const MAX_POINTS: u32 = 10000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS)
+}
---
+++ 02-var/shadowing.rs
@@ -0,0 +1,37 @@
+fn main(){
+    // let x = 5;
+    // println!("The value of x is: {}", x);  
+
+    // {
+    //     let x = 8;
+    //     println!("The value of x is: {}", x);  
+
+    //     let x = x + 2;
+    //     println!("The value of x is: {}", x); 
+    // }
+
+    // let x = 6;
+    // println!("The value of x is: {}", x);
+
+//      The value of x is: 5
+//      The value of x is: 8
+//      The value of x is: 10
+//      The value of x is: 6
+
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x); 
+
+    // {
+    //     x = 10;
+    //     println!("The value of x is: {}", x); 
+    // }
+
+    // let x = x + 2;
+    // println!("The value of x is: {}", x); 
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = "hello";
+    println!("The value of x is: {}", x);
+}
